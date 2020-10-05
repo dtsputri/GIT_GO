63ab5f6af858454e96ff98e5cebb059e32eadfa3
Putri <dtsputrirosimayasari@outlook.com>
2020-10-05 20:02:38 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+index.php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset = "UTF-8">
+    <meta name="viewport" content="width=device-width, inital-scale=.10">
+    <title> latihan GIT Remote </title>
+</head>
+<body>
+    <h1> hello world</h1>
+</body>
+</html>
